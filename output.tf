5bc61ad2a71e588680e569eb189506614ad55255
uns-miyama <miyama@uniss.co.jp>
2022-09-22 16:28:35 +0900

    changed output

--- output.tf
+++ output.tf
@@ -1,3 +1,15 @@
-output "name" {
+output "eip" {
+  description = "AWSインスタンスのパブリックIPを表示"
   value = aws_eip.hashicat.public_ip
+}
+
+output "instance-info" {
+  description = "AWSインスタンスの情報を表示"
+  value = [
+    aws_instance.hashicat.arn,
+    aws_instance.hashicat.availability_zone,
+    aws_instance.hashicat.cpu_core_count,
+    aws_instance.hashicat.private_ip,
+    aws_instance.hashicat.instance_type
+    ]
 }
